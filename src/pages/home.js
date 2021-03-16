b8e97a6fd9529cb2e66958da19d8026e30b279fb
Simona Trotti <simona.trotti.external@ynap.com>
2021-03-16 22:31:13 +0100

    image in home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,24 @@
-import * as React from "react";
-
-export default function Home() {
-  return <div>HOME</div>;
+import React, { Component } from "react";
+import Image from "../components/image";
+class Home extends Component {
+  render() {
+    return (
+      <div
+        className="imageContainer"
+        style={{ padding: "2%", border: "2px solid #69838d" }}
+      >
+        <Image
+          src="muffins.png"
+          className="mx-auto shadow-xl"
+          alt="muffins Image"
+          style={{
+            width: "100%",
+            height: "500px",
+          }}
+        />
+      </div>
+    );
+  }
 }
+
+export default Home;
