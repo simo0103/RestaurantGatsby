65dfa9b01700a58fee9a88cecb8dfeb98ee51edc
Simona Trotti <simona.trotti.external@ynap.com>
2021-03-22 21:57:29 +0100

    cakes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Image from "../components/image";
 import { graphql, useStaticQuery } from "gatsby";
 
@@ -17,22 +17,65 @@ export default function Home() {
     }
   `);
   return (
-    <div className="imageContainer">
-      <div className="announcement">
-        {data.allContentfulSliderHome.edges.map((ann) => (
-          <p>{ann.node.announcement}</p>
-        ))}
-        <button>explore</button>
+    <Fragment>
+      <div className="imageContainer">
+        <div className="announcement">
+          {data.allContentfulSliderHome.edges.map((ann) => (
+            <p>{ann.node.announcement}</p>
+          ))}
+          <button>explore</button>
+        </div>
+        <Image
+          src="cupcakes2.jpg"
+          className="mx-auto shadow-xl"
+          alt="muffins Image"
+          style={{
+            width: "100%",
+            height: "700px",
+          }}
+        />
       </div>
-      <Image
-        src="cupcakes2.jpg"
-        className="mx-auto shadow-xl"
-        alt="muffins Image"
-        style={{
-          width: "100%",
-          height: "700px",
-        }}
-      />
-    </div>
+      <div className="bestSellers">
+        <h3>best sellers of this week</h3>
+        <div class="container">
+          <div>
+            <Image
+              src="cake_1920.jpg"
+              className="mx-auto shadow-xl"
+              alt="muffins Image"
+              style={{
+                width: "80%",
+                margin: "0 auto",
+                height: "400px",
+              }}
+            />
+          </div>
+          <div>
+            <Image
+              src="darkcake_1920.jpg"
+              className="mx-auto shadow-xl"
+              alt="muffins Image"
+              style={{
+                width: "80%",
+                margin: "0 auto",
+                height: "400px",
+              }}
+            />
+          </div>
+          <div>
+            <Image
+              src="cake_1920.jpg"
+              className="mx-auto shadow-xl"
+              alt="muffins Image"
+              style={{
+                width: "80%",
+                margin: "0 auto",
+                height: "400px",
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
