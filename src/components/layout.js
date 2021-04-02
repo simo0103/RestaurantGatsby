4a601e51987542bcc09062aef75ae78b9a5d1ede
Simona Trotti <simona.trotti.external@ynap.com>
2021-04-02 23:07:47 +0200

    layout functional component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,42 +1,41 @@
 import * as React from "react";
-import { StaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 import { Helmet } from "react-helmet";
 import Navbar from "./navbar";
 import "@fontsource/open-sans";
 import "../style/style.scss";
 
-const Layout = ({ children }) => (
-  // StaticQuery is a new component introduced in Gatsby V2,
-  // which allows you to run GraphQL queries within your components,
-  // not just pages.It allows developers to collocate data with their components.
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-            menuLinks {
-              name
-              link
-            }
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+          menuLinks {
+            name
+            link
           }
         }
       }
-    `}
-    render={(data) => (
-      <React.Fragment>
-        <Helmet title={data.site.siteMetadata.title}></Helmet>
-        <header>
-          <h1>{data.site.siteMetadata.title}</h1>
-          <Navbar
-            menuLinks={data.site.siteMetadata.menuLinks}
-            siteTitle={data.site.siteMetadata.title}
-          ></Navbar>
-        </header>
-        <main>{children}</main>
-      </React.Fragment>
-    )}
-  />
-);
+    }
+  `);
+  // StaticQuery is a new component introduced in Gatsby V2,
+  // which allows you to run GraphQL queries within your components,
+  // not just pages.It allows developers to collocate data with their components.
+
+  return (
+    <React.Fragment>
+      <Helmet title={data.site.siteMetadata.title}></Helmet>
+      <header>
+        <h1>{data.site.siteMetadata.title}</h1>
+        <Navbar
+          menuLinks={data.site.siteMetadata.menuLinks}
+          siteTitle={data.site.siteMetadata.title}
+        ></Navbar>
+      </header>
+      <main>{children}</main>
+    </React.Fragment>
+  );
+};
 
 export default Layout;
