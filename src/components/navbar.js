1526b80839610c43602337efe2dc053c3316079d
Simona Trotti <simona.trotti.external@ynap.com>
2021-04-02 22:28:57 +0200

    navbar functional component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,58 +1,42 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.toggleClass = this.toggleClass.bind(this);
+function Navbar(props) {
+  const [clicked, setClicked] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    this.state = {
-      clicked: false,
-      visible: false,
-    };
-  }
-  toggleClass() {
-    const currentState = this.state.clicked;
+  const toggleClass = () => {
+    setClicked(!clicked);
+    setVisible(!visible);
+
     console.log("menu closed");
-    this.setState({
-      clicked: !currentState,
-      visible: !currentState,
-    });
-  }
+  };
 
-  render() {
-    return (
-      <nav>
-        <div
-          id="hamburger"
-          className={this.state.clicked ? "clicked" : null}
-          onClick={this.toggleClass}
-        >
-          <span className="firstLine"></span>
-          <span className="secondLine"></span>
-          <span className="thirdLine"></span>
-        </div>
-        <ul className={this.state.visible ? "visible" : null}>
-          {this.props.menuLinks.map((link) => (
-            <li className={link.name} key={link.name}>
-              <Link to={link.link}>{link.name}</Link>
-            </li>
-          ))}
-        </ul>
-        <div className="mobilePanelContainer">
-          <div
-            id="panelLeft"
-            className={this.state.visible ? "visible" : null}
-          />
-          <div
-            id="panelRight"
-            className={this.state.visible ? "visible" : null}
-          />
-        </div>
-      </nav>
-    );
-  }
+  return (
+    <nav>
+      <div
+        id="hamburger"
+        className={clicked ? "clicked" : null}
+        onClick={toggleClass}
+      >
+        <span className="firstLine"></span>
+        <span className="secondLine"></span>
+        <span className="thirdLine"></span>
+      </div>
+      <ul className={visible ? "visible" : null}>
+        {props.menuLinks.map((link) => (
+          <li className={link.name} key={link.name}>
+            <Link to={link.link}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <div className="mobilePanelContainer">
+        <div id="panelLeft" className={visible ? "visible" : null} />
+        <div id="panelRight" className={visible ? "visible" : null} />
+      </div>
+    </nav>
+  );
 }
 
 Navbar.propTypes = {
