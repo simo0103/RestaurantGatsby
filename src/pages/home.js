77e50d8055156ff10d2083d2c6ce821ac35789d2
Simona Trotti <simona.trotti.external@ynap.com>
2021-04-05 19:41:59 +0200

    second image in home

--- src/pages/home.js
+++ src/pages/home.js
@@ -19,6 +19,8 @@ export default function Home() {
   return (
     <Fragment>
       <div className="imageContainer">
+        <span className="overlay"></span>
+
         {/* <div className="announcement">
           {data.allContentfulSliderHome.edges.map((ann) => (
             <p key={ann.node.announcement}>{ann.node.announcement}</p>
@@ -27,6 +29,29 @@ export default function Home() {
         </div> */}
         <Image
           src="burger.jpg"
+          className="mx-auto shadow-xl"
+          alt="muffins Image"
+          style={{
+            position: "absolute",
+            overflow: "hidden",
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            top: 0,
+            left: 0,
+          }}
+        />
+      </div>
+      <div className="imageContainer">
+        <span className="overlay"></span>
+        {/* <div className="announcement">
+          {data.allContentfulSliderHome.edges.map((ann) => (
+            <p key={ann.node.announcement}>{ann.node.announcement}</p>
+          ))}
+          <button>explore</button>
+        </div> */}
+        <Image
+          src="sandwich.jpg"
           className="mx-auto shadow-xl"
           alt="muffins Image"
           style={{
